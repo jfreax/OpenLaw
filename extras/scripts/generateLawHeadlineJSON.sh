814730104a7a9cbd281c049c3c21a6c3fcac1070
Jens Dieskau <jfreax@googlemail.com>
2012-10-11 17:12:57 +0200

    Script to generate server "database".

---
+++ extras/scripts/generateLawHeadlineJSON.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+gesetzePath="/home/jens/VCS/gesetze/"
+outputPath="/home/jens/Dropbox/Programmierung/Java/Android/Gesetze/extras/server data/law/"
+
+
+while read s
+do 
+  name=$(grep "^slug:" $s | sed 's/slug: //g' | tr -d \\n)
+  
+  echo -n "" > "$outputPath/$name"
+  
+  grep "^#" $s | sed 's/"/\\"/g' | while read line
+  do
+    if [ $(echo $line | grep -c "^# " ) -gt 0 ]; then
+      echo $line | sed 's/^# /1:/g' >> "$outputPath/$name"
+    elif [ $(echo $line | grep -c "^## " ) -gt 0 ]; then
+      echo $line | sed 's/^## /2:/g' >> "$outputPath/$name"
+    elif [ $(echo $line | grep -c "^### " ) -gt 0 ]; then
+      echo $line | sed 's/^### /3:/g' >> "$outputPath/$name"
+    elif [ $(echo $line | grep -c "^#### " ) -gt 0 ]; then
+      echo $line | sed 's/^#### /4:/g' >> "$outputPath/$name"
+    elif [ $(echo $line | grep -c "^##### " ) -gt 0 ]; then
+      echo $line | sed 's/^##### /5:/g' >> "$outputPath/$name"
+    elif [ $(echo $line | grep -c "^###### " ) -gt 0 ]; then
+      echo $line | sed 's/^###### /6:/g' >> "$outputPath/$name"
+    fi
+  done
+
+#   name=$(grep "^slug:" $s | sed 's/slug: //g' | tr -d \\n)
+#   echo "[" > "$outputPath/$name"
+# 
+#   # Headline 1
+#   first=1
+#   echo -n "{1: {" >> "$outputPath/$name"
+#   grep "^# " $s | sed 's/^# //g' | sed 's/"/\\"/g' | while read headline
+#   do
+#     if [ $first -eq 0 ]; then
+#       echo -n "," >> "$outputPath/$name"
+#     else
+#       first=0
+#     fi
+#     echo -n "\"$headline\"" >> "$outputPath/$name"
+#   done
+#   echo "}], " >> "$outputPath/$name"
+# 
+#   # Headline 2
+#   first=1
+#   echo -n "{2: {" >> "$outputPath/$name"
+#   grep "^## " $s | sed 's/^## //g' | sed 's/"/\\"/g' | while read headline
+#   do
+#     if [ $first -eq 0 ]; then
+#       echo -n "," >> "$outputPath/$name"
+#     else
+#       first=0
+#     fi
+#     echo -n "\"$headline\"" >> "$outputPath/$name"
+#   done
+#   echo "}]" >> "$outputPath/$name"
+# 
+#   # Headline 3
+#   first=1
+#   echo -n "{3: {" >> "$outputPath/$name"
+#   grep "^### " $s | sed 's/^### //g' | sed 's/"/\\"/g' | while read headline
+#   do
+#     if [ $first -eq 0 ]; then
+#       echo -n "," >> "$outputPath/$name"
+#     else
+#       first=0
+#     fi
+#     echo -n "\"$headline\"" >> "$outputPath/$name"
+#   done
+#   echo "}]" >> "$outputPath/$name"
+#   
+#   echo "]" >> "$outputPath/$name"
+#   
+done < <(find $gesetzePath -iname index.md)
+
+
---
+++ extras/scripts/generateLawNamesJSON.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export gesetzePath="/home/jens/VCS/gesetze/"
+
+echo "["
+
+while read s
+do 
+  echo -n "[\""
+  grep "^jurabk:" $s | sed 's/jurabk: //g' | tr -d \\n
+  echo -n "\", \""
+  grep "^slug:" $s | sed 's/slug: //g' | tr -d \\n
+  echo -n "\", \""
+  cat $s | head -2 | tail -1 | sed 's/Title: //g' | sed 's/"/\\"/g' | tr -d \\n
+  echo "\"],"
+done < <(find $gesetzePath -iname index.md)
+
+echo "]"
