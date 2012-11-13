d417a3d39a75eb0600c76c62d3f4165e4fab9bcd
Jens Dieskau <jfreax@googlemail.com>
2012-11-13 19:18:45 +0100

    Script to download all laws (law text only!).

---
+++ extras/scripts/downloadGesetze.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import lxml.etree
+import lxml.html
+
+output_dir = "./out/"
+base_url = "http://www.gesetze-im-internet.de/"
+root_alphabet = lxml.html.parse(base_url+"aktuell.html").getroot()
+
+def getAllLaws():
+    ret = []
+
+    alphabet = root_alphabet.cssselect("#container .alphabet")
+    for el in alphabet:
+        if el.attrib.has_key('href'):
+            alphabet_elem = lxml.html.parse(base_url+el.attrib['href']).getroot()
+
+            title_elem = alphabet_elem.cssselect("#paddingLR12 p a")
+
+            for el_title in title_elem:
+                if el_title.attrib.has_key('href') and not el_title.attrib['href'].endswith('.pdf'):
+                    ret.append(el_title.attrib['href'][2:-11])
+                    #print el_title.getchildren()[0].text, el_title.attrib['href']
+
+            return ret
+    return ret
+
+
+def getLawText(law):
+    law_root = lxml.html.parse(base_url+law+"/index.html").getroot()
+    head_elem = law_root.cssselect("#paddingLR12 td a")
+
+    i = 0
+    for el in head_elem:
+        if el.attrib.has_key('href'):
+            head_link = el.attrib['href']
+            head_root = lxml.html.parse(base_url+law+"/"+head_link).getroot()
+
+            for bad in head_root.xpath("//a[text()='Nichtamtliches Inhaltsverzeichnis']"):
+                bad.getparent().remove(bad)
+            for bad in head_root.xpath("//div[contains(@class, 'jnheader')]"):
+                bad.getparent().remove(bad)
+
+            headHtml_elem = head_root.cssselect("#paddingLR12")
+
+            directory = output_dir + '/' + law + '/'
+
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            with open(directory + str(i), 'w') as lawFile:
+                lawFile.write(lxml.etree.tostring(headHtml_elem[0]))
+
+            i = i+1
+
+
+
+laws = getAllLaws()
+for law in laws:
+    getLawText(law)
