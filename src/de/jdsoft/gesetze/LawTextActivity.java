1ae9cd2794266fb0e311b6cffec44579f555fa29
Jens Dieskau <jfreax@googlemail.com>
2012-11-14 22:22:00 +0100

    Law text activity (stub).

---
+++ src/de/jdsoft/gesetze/LawTextActivity.java
@@ -0,0 +1,13 @@
+package de.jdsoft.gesetze;
+
+import android.os.Bundle;
+
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class LawTextActivity extends SherlockFragmentActivity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ src/de/jdsoft/gesetze/LawTextFragment.java
@@ -0,0 +1,18 @@
+package de.jdsoft.gesetze;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class LawTextFragment extends SherlockFragment {
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public LawTextFragment() {
+	}
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
