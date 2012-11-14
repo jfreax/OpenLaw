6c82536e62e78dea49353b60e628692edb53308e
Jens Dieskau <jfreax@googlemail.com>
2012-11-14 11:11:21 +0100

    New folder structure for law headlines.

--- extras/scripts/generateLawHeadlineJSON.sh
+++ extras/scripts/generateLawHeadlineJSON.sh
@@ -6,73 +6,27 @@ outputPath=$1
 while read s
 do 
   name=$(grep "^origslug:" $s | sed 's/origslug: //g' | sed 's/-/_/g' | tr -d \\n)
+  filename="${outputPath}/${name}/heads"
   
-  echo -n "" > "$outputPath/$name"
+  echo -n "" > "$filename"
   
   grep "^#" $s | sed 's/"/\\"/g' | while read line
   do
     if [ $(echo $line | grep -c "^# " ) -gt 0 ]; then
-      echo $line | sed 's/^# /1:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^# /1:/g' >> "$filename"
     elif [ $(echo $line | grep -c "^## " ) -gt 0 ]; then
-      echo $line | sed 's/^## /2:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^## /2:/g' >> "$filename"
     elif [ $(echo $line | grep -c "^### " ) -gt 0 ]; then
-      echo $line | sed 's/^### /3:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^### /3:/g' >> "$filename"
     elif [ $(echo $line | grep -c "^#### " ) -gt 0 ]; then
-      echo $line | sed 's/^#### /4:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^#### /4:/g' >> "$filename"
     elif [ $(echo $line | grep -c "^##### " ) -gt 0 ]; then
-      echo $line | sed 's/^##### /5:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^##### /5:/g' >> "$filename"
     elif [ $(echo $line | grep -c "^###### " ) -gt 0 ]; then
-      echo $line | sed 's/^###### /6:/g' >> "$outputPath/$name"
+      echo $line | sed 's/^###### /6:/g' >> "$filename"
     fi
   done
 
-#   name=$(grep "^slug:" $s | sed 's/slug: //g' | tr -d \\n)
-#   echo "[" > "$outputPath/$name"
-# 
-#   # Headline 1
-#   first=1
-#   echo -n "{1: {" >> "$outputPath/$name"
-#   grep "^# " $s | sed 's/^# //g' | sed 's/"/\\"/g' | while read headline
-#   do
-#     if [ $first -eq 0 ]; then
-#       echo -n "," >> "$outputPath/$name"
-#     else
-#       first=0
-#     fi
-#     echo -n "\"$headline\"" >> "$outputPath/$name"
-#   done
-#   echo "}], " >> "$outputPath/$name"
-# 
-#   # Headline 2
-#   first=1
-#   echo -n "{2: {" >> "$outputPath/$name"
-#   grep "^## " $s | sed 's/^## //g' | sed 's/"/\\"/g' | while read headline
-#   do
-#     if [ $first -eq 0 ]; then
-#       echo -n "," >> "$outputPath/$name"
-#     else
-#       first=0
-#     fi
-#     echo -n "\"$headline\"" >> "$outputPath/$name"
-#   done
-#   echo "}]" >> "$outputPath/$name"
-# 
-#   # Headline 3
-#   first=1
-#   echo -n "{3: {" >> "$outputPath/$name"
-#   grep "^### " $s | sed 's/^### //g' | sed 's/"/\\"/g' | while read headline
-#   do
-#     if [ $first -eq 0 ]; then
-#       echo -n "," >> "$outputPath/$name"
-#     else
-#       first=0
-#     fi
-#     echo -n "\"$headline\"" >> "$outputPath/$name"
-#   done
-#   echo "}]" >> "$outputPath/$name"
-#   
-#   echo "]" >> "$outputPath/$name"
-#   
 done < <(find $gesetzePath -iname index.md)
 
 
