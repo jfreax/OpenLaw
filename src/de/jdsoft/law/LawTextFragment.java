4e8945ba87d31e50ffcb192535b8f343a3a6614e
Jens Dieskau <jfreax@googlemail.com>
2013-07-15 22:17:17 +0200

    Method to start selection in webview for law text activity.

--- src/de/jdsoft/law/LawTextFragment.java
+++ src/de/jdsoft/law/LawTextFragment.java
@@ -1,52 +1,36 @@
 package de.jdsoft.law;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.webkit.WebView;
-
 import android.widget.LinearLayout;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.jakewharton.DiskLruCache.Editor;
 import com.jakewharton.DiskLruCache.Snapshot;
 import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import com.viewpagerindicator.PageIndicator;
-import com.viewpagerindicator.TabPageIndicator;
 import de.jdsoft.law.data.Cache;
 import de.jdsoft.law.helper.TweakedWebView;
 import de.jdsoft.law.network.RestClient;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class LawTextFragment extends SherlockFragment {
     public static final String ARG_ITEM_ID = "text_id";
     public static final String ARG_ITEM_SLUG = "law";
     public static final String ARG_ITEM_SHORT = "shortname";
-
     private Cache cache = null;
-
     private long id = 0;
     private String slug = "";
-
     private TweakedWebView webview = null;
     private LinearLayout loading = null;
     private LinearLayout text_overlay = null;
-
     private String lawText = "";
 
 
@@ -72,7 +56,7 @@ public class LawTextFragment extends SherlockFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if ( getArguments() != null && getArguments().containsKey(ARG_ITEM_ID)
+        if (getArguments() != null && getArguments().containsKey(ARG_ITEM_ID)
                 && getArguments().containsKey(ARG_ITEM_SLUG)) {
 
             id = getArguments().getLong(ARG_ITEM_ID);
@@ -99,21 +83,20 @@ public class LawTextFragment extends SherlockFragment {
             }
         });
 
-        loading = (LinearLayout)rootView.findViewById(R.id.loading);
-        text_overlay = (LinearLayout)rootView.findViewById(R.id.text_overlay);
+        loading = (LinearLayout) rootView.findViewById(R.id.loading);
+        text_overlay = (LinearLayout) rootView.findViewById(R.id.text_overlay);
 
         // Set touch listener to show button overlay
         rootView.setOnTouchListener(new View.OnTouchListener() {
 
             public AnimRunnable fadeOutRunner = null;
             public Handler mHandler = new Handler();
-
             public boolean isFaded = false;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if( fadeOutRunner == null ) {
-                    if ( !isFaded ) {
+                if (fadeOutRunner == null) {
+                    if (!isFaded) {
                         final Animation animFadeIn = AnimationUtils.loadAnimation(getContext(), android.R.anim.fade_in);
                         text_overlay.setAnimation(animFadeIn);
                         text_overlay.setVisibility(View.VISIBLE);
@@ -123,7 +106,7 @@ public class LawTextFragment extends SherlockFragment {
                     mHandler.postDelayed(fadeOutRunner = new AnimRunnable() {
                         @Override
                         public void run() {
-                            if( isStop() ) {
+                            if (isStop()) {
                                 return;
                             }
                             final Animation animFadeOut = AnimationUtils.loadAnimation(getContext(), android.R.anim.fade_out);
@@ -149,14 +132,25 @@ public class LawTextFragment extends SherlockFragment {
         return rootView;
     }
 
+    public void StartSelectText() {
+        try {
+            KeyEvent shiftPressEvent =
+                    new KeyEvent(0, 0, KeyEvent.ACTION_DOWN,
+                            KeyEvent.KEYCODE_SHIFT_LEFT, 0, 0);
+            shiftPressEvent.dispatch(webview);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
     private void LoadOrCache() {
         // Try to read from cache
         try {
-            if ( cache == null || cache.isClosed() ) {
+            if (cache == null || cache.isClosed()) {
                 cache.openCache();
             }
-            Snapshot snapshot = cache.get(slug+"_"+id);
-            if ( snapshot != null ) {
+            Snapshot snapshot = cache.get(slug + "_" + id);
+            if (snapshot != null) {
                 lawText = snapshot.getString(0);
                 reloadData(false);
                 return;
@@ -168,18 +162,18 @@ public class LawTextFragment extends SherlockFragment {
         // Not in cache, try to read from network
         RestClient.get(getContext(), slug + "/" + id, null, new AsyncHttpResponseHandler() {
             public void onSuccess(String response) {
-                Log.i("GetLawText", "onSuccess() Response size: "+response.length());
-                if ( response.length() == 0 ) {
+                Log.i("GetLawText", "onSuccess() Response size: " + response.length());
+                if (response.length() == 0) {
                     Log.e(LawTextFragment.class.getName(), "Can't download law " + slug + " " + id);
                     return;
                 }
 
                 // Save to cache
                 try {
-                    if ( cache == null || cache.isClosed() ) {
+                    if (cache == null || cache.isClosed()) {
                         cache.openCache();
                     }
-                    Editor creator = cache.edit(slug+"_"+id);
+                    Editor creator = cache.edit(slug + "_" + id);
                     creator.set(0, response);
                     creator.commit();
                     cache.flush();
@@ -201,12 +195,12 @@ public class LawTextFragment extends SherlockFragment {
     }
 
     private void reloadData(boolean isError) {
-        if( isError || getSherlockActivity() == null ) {
+        if (isError || getSherlockActivity() == null) {
             return;
         }
 
         // Only valid text data
-        if( lawText.length() <= 5 || lawText.substring(0, 4).contains("%") ) {
+        if (lawText.length() <= 5 || lawText.substring(0, 4).contains("%")) {
             // if not valid, try next
             id++;
             LoadOrCache();
@@ -214,17 +208,17 @@ public class LawTextFragment extends SherlockFragment {
         }
 
         // Select correct item in listview to visualize current selected
-        if( getSherlockActivity() instanceof LawListActivity) { // Only in two pane mode
-            if( ((LawListActivity)getSherlockActivity()).headlineFragment == null ) { // == activity cancelled
+        if (getSherlockActivity() instanceof LawListActivity) { // Only in two pane mode
+            if (((LawListActivity) getSherlockActivity()).headlineFragment == null) { // == activity cancelled
                 return;
             }
-            ((LawListActivity)getSherlockActivity()).headlineFragment.getListView().setItemChecked((int)id, true);
+            ((LawListActivity) getSherlockActivity()).headlineFragment.getListView().setItemChecked((int) id, true);
         }
 
-        if ( webview != null ) {
+        if (webview != null) {
             String html = "<html><body bgcolor=\"#eee\">" + lawText + "</body></html>";
             try {
-                webview.loadData(URLEncoder.encode(html, "utf-8").replaceAll("\\+"," "), "text/html", "utf-8");
+                webview.loadData(URLEncoder.encode(html, "utf-8").replaceAll("\\+", " "), "text/html", "utf-8");
                 // Show webview
                 webview.setVisibility(View.VISIBLE);
                 // Hide loading animation
